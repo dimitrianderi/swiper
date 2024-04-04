2592dd6894653241b2d1399ddf2fcd1c5a179997
Dmitry Kharitonov <dimitrianderi@gmail.com>
2024-04-04 11:20:02 +0500

    feat

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import * as path from 'path';
 import vue from '@vitejs/plugin-vue'
-import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
   base: "/",
-  plugins: [vue(), eslintPlugin()],
+  plugins: [vue()],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
